3e71be32095c6a8a72d9106c2e0dd0261d30197e
Matheus Cunha Reis <matheuscunhareis30@gmail.com>
2018-06-21 22:36:52 -0300

    Fix Bug

--- app.js
+++ app.js
@@ -13,6 +13,4 @@ ALWAYS DO THAT
 
 1 - export GOOGLE_APPLICATION_CREDENTIALS=Small-Talk-9e39e7f52b88.json
 
-2 - change node_modules/node-spotify-webhelper/index.js - line 158: return util.format("http://%s:%d%s", generateRandomLocalHostName(), localPort, url);
-
  */
